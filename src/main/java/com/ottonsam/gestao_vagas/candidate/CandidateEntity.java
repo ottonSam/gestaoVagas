b86f7faf1f6e8e772e7887f74ce6b12522b06d4d
ottonsam <ottonsam@gmail.com>
2025-04-06 11:24:35 -0300

    feat: Adicionar configuração do Docker e dependências do PostgreSQL, além de atualizar a entidade Candidate com timestamp de criação

--- src/main/java/com/ottonsam/gestao_vagas/candidate/CandidateEntity.java
+++ src/main/java/com/ottonsam/gestao_vagas/candidate/CandidateEntity.java
@@ -1,19 +1,30 @@
 package com.ottonsam.gestao_vagas.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
+    @NotBlank(message = "Username cannot be empty")
     @Pattern(regexp = "\\S+", message = "Username cannot be spaces")
     private String username;  
 
@@ -25,4 +36,6 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
     
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
