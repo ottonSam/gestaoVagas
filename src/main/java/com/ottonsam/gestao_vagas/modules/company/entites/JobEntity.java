87c7dd2778e149d74587399e138c0edfef920a53
ottonsam <ottonsam@gmail.com>
2025-04-06 13:46:07 -0300

    fix: Adicionar coluna companyId à JobEntity e ajustar relacionamento com CompanyEntity

--- src/main/java/com/ottonsam/gestao_vagas/modules/company/entites/JobEntity.java
+++ src/main/java/com/ottonsam/gestao_vagas/modules/company/entites/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -25,8 +26,11 @@ public class JobEntity {
     private String level;
 
     @ManyToOne()
-    @JoinColumn(name = "company_id")
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity company;
+    
+    @Column(name = "company_id")
+    private UUID companyId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
