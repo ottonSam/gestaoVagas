339ec8afbc3cdc81d3a51e9fb0bb395a96f93ea7
ottonsam <ottonsam@gmail.com>
2025-04-14 17:27:57 -0300

    feat: Adicionar expiração ao token JWT na autenticação de empresas

--- src/main/java/com/ottonsam/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/ottonsam/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package com.ottonsam.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +40,7 @@ public class AuthCompanyUseCase {
         Algorithm algorithm = Algorithm.HMAC256(secreteToken);
         
         var token = JWT.create().withIssuer("gestao-vagas")
+            .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
             .withSubject(company.getId().toString())
             .sign(algorithm);
     
